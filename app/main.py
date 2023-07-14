cd1003cd754fb2ab36b38d25e3c8d3318b760f77
tasnim <tasnim0tantawi@gmail.com>
2023-07-14 16:05:46 +0300

    server initialize

--- app/main.py
+++ app/main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI, status, HTTPException
 from pydantic import BaseModel
-from pydantic.types import Optional
+from typing import Optional
 from app.dummy import all_posts, search_post, search_post_index
 
 
@@ -15,20 +15,52 @@ class Post(BaseModel):
     published: bool = True
     rating: Optional[int] = None
 
-
+# The function root gets called when the user visits the root of the API.
 @app.get("/")
 def root():
     return {"message": "Hi Tasnim!"}
 
+
+# Getting all posts, a best practice is to name the route /posts with an s at the end.
 @app.get("/posts")
 def get_posts():
     return {
         "data": posts
     }
 
-@app.post("/posts/")
-def create_post():
-    pass
+@app.post("/posts")
+def create_post(post: Post):
+    posts.append(post.model_dump())
+    return {
+        "message": "Post created successfully.",
+        "data": posts
+    }
+
+@app.put("/posts/{id}")
+def update_post(id: int, post: Post):
+    post_index = search_post_index(id)
+    if post_index is None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Post not found.")
+    posts[post_index] = post
+    return {
+        "message": "Post updated successfully.",
+        "data": posts
+    }
+
+@app.delete("/posts/{id}", status_code=status.HTTP_204_NO_CONTENT)
+def delete_post(id):
+    post_index = search_post_index(id)
+    if post_index is None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Post not found.")
+    del posts[post_index]
+    return {
+        "message": "Post deleted successfully.",
+        "data": posts
+    }
+
+
+
+    
 
 
 @app.put("/posts/{id}", status_code=status.HTTP_200_OK)
