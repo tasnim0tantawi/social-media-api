f92c9d658e62e4a8cd504818d68177f870ca442e
tasnim <tasnim0tantawi@gmail.com>
2023-07-21 18:13:59 +0300

    sql alchemy

---
+++ app/database.py
@@ -0,0 +1,10 @@
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+from .secret import db_password, db_name
+
+SQLALCHEMY_DATABASE_URL = f"postgresql://postgres:{db_password}@localhost/{db_name}"
+
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
+session_local = sessionmaker(autocommit=False, autoflush=False, bind=engine)
