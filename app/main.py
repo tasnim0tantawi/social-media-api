5534c9c1505821a488a6734503bc062d0fff33b6
tasnim <tasnim0tantawi@gmail.com>
2023-08-17 08:30:29 +0300

    adding comments

--- app/main.py
+++ app/main.py
@@ -4,7 +4,7 @@ from . database import engine
 from . routers import post, user, authentication, reaction
 
 
-models.Base.metadata.create_all(bind=engine)
+# models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 
